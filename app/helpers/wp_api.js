44b1d79221bcf72ad42233cb5782ed5a5e3dfe87
kapelu <ddaniel_calderon@hotmail.com>
2024-04-20 09:26:54 -0300

    Add: Inicio de proyecto

---
+++ app/helpers/wp_api.js
@@ -0,0 +1,22 @@
+const NAME = 'Noticias - CSS'
+const DOMAIN = `https:/css-tricks.com`;
+const SITE = `${DOMAIN}/wp-json`
+const API_WP = `${SITE}/wp/v2`
+const PER_PAGE = 9
+const POSTS = `${API_WP}/posts?_embed&per_page=${PER_PAGE}`
+const POST = `${API_WP}/posts`
+const SEARCH = `${API_WP}/search?_embed&per_page=${PER_PAGE}&search=`
+
+const page = 1
+
+export default {
+    NAME,
+    DOMAIN,
+    SITE,
+    API_WP,
+    PER_PAGE,
+    POSTS,
+    POST,
+    SEARCH,
+    page
+}
