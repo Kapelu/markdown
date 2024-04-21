829b556e63b61025c608f85243b1ab681a7deecc
kapelu <ddaniel_calderon@hotmail.com>
2024-04-20 23:26:26 -0300

    update

--- app/components/Router.js
+++ app/components/Router.js
@@ -13,54 +13,55 @@ export async function Router() {
 
 	$main.innerHTML = null
 
-	if (!hash || hash === '#/') {
-		await ajax({
-			url: api.POSTS,
-			cbSuccess: (posts) => {
-				//console.log(posts)
-				let html = ''
-				posts.forEach((post) => (html += PostCard(post)))
-				$main.innerHTML = html
-			},
-		})
-		//console.log(api.POST);
-	} else if (hash.includes('#/search')) {
-		let query = localStorage.getItem('wpSearch')
-		if(!query){
-			document.querySelector('.loader').style.display = 'none'
-			return false
-		}
-		await ajax({
-			url: `${api.SEARCH}${query}`,
-			cbSuccess: (search) => {
-				//console.log(search)
-				let html = ''
-				if(search.length === 0){
-					html = `
+	if (!hash || hash === "https://kapelu.github.io/single-page-css/") {
+    await ajax({
+      url: api.POSTS,
+      cbSuccess: (posts) => {
+        //console.log(posts)
+        let html = "";
+        posts.forEach((post) => (html += PostCard(post)));
+        $main.innerHTML = html;
+      },
+    });
+    //console.log(api.POST);
+  } else if (hash.includes("#/search")) {
+    let query = localStorage.getItem("wpSearch");
+    if (!query) {
+      document.querySelector(".loader").style.display = "none";
+      return false;
+    }
+    await ajax({
+      url: `${api.SEARCH}${query}`,
+      cbSuccess: (search) => {
+        //console.log(search)
+        let html = "";
+        if (search.length === 0) {
+          html = `
 					<p class='error'>
 						No existen resultado de busqueda para el término
 						<mark>${query}</mark>
 					</p>
-					`
-				} else {
-					search.forEach(post => html+= SearchCard(post))
-				}
-				$main.innerHTML = html
-			},
-		})
-	} else if (hash === '#/contact') {		
-		$main.appendChild(ContactForm())
-	} else {
-		$main.innerHTML ='<h2>Aquí cargara el contenido del posts previamente seleccionado</h2>'
+					`;
+        } else {
+          search.forEach((post) => (html += SearchCard(post)));
+        }
+        $main.innerHTML = html;
+      },
+    });
+  } else if (hash === "#/contact") {
+    $main.appendChild(ContactForm());
+  } else {
+    $main.innerHTML =
+      "<h2>Aquí cargara el contenido del posts previamente seleccionado</h2>";
 
-		await ajax({
-			url: `${api.POST}/${localStorage.getItem('wpPostId')}`,
-			cbSuccess: (post) => {
-				console.log(post)
-				$main.innerHTML = Post(post)
-			},
-		})
-}
+    await ajax({
+      url: `${api.POST}/${localStorage.getItem("wpPostId")}`,
+      cbSuccess: (post) => {
+        console.log(post);
+        $main.innerHTML = Post(post);
+      },
+    });
+  }
 
 	document.querySelector('.loader').style.display = 'none'
 }
--- app/index.js
+++ app/index.js
@@ -1,8 +1,8 @@
-import api from './helpers/wp_api.js'
-import { App } from './App.js';
+import api from "./helpers/wp_api.js";
+import { App } from "./App.js";
 
-document.addEventListener('DOMContentLoaded', App)
-window.addEventListener('hashchange', ()=>{
-    api.page=1
-    App()
-})
+document.addEventListener("DOMContentLoaded", App);
+window.addEventListener("hashchange", () => {
+    api.page = 1;
+    App();
+});
